5e1094b07ef0316e1ca9776502598c518d6c1920
Yair Pimentel <42229163+Yair0@users.noreply.github.com>
2020-03-19 15:50:50 -0600

    Splash Screen

---
+++ app/src/main/java/mx/yair/discipulos_app/SplashScreen.kt
@@ -0,0 +1,25 @@
+package mx.yair.discipulos_app
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import kotlinx.android.synthetic.main.splash_screen.*
+
+class SplashScreen : AppCompatActivity() {
+    private val  TIME: Long = 3000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+        Handler().postDelayed(
+            {
+                startActivity(
+                    Intent(this, MainActivity::class.java)
+                )
+            },TIME)
+
+        appName.text = "Aplicacion desarrollada para resolver el problema del X"
+        namesAlumno.text = "Yair Pimentel\t\tA01652823\n\nGerman Reyes\t\tA01336637\n\nDiego Moreno\t\tA01"
+    }
+}
